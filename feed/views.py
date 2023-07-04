6dc016bdb9d0454d9f146d7da1f690cc77b1c4ba
Julius Walton <juliuswalton7901@gmail.com>
2023-07-04 00:18:52 -0400

    Refactored file to make it easier to read and understnad

--- feed/views.py
+++ feed/views.py
@@ -12,6 +12,7 @@ class YouTubeUserVideos(View):
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def find_channel_by_custom_url(self, custom_url, max_results=10):
+        """Find a YouTube channel by its custom URL."""
         try:
             resp = self.youtube.search().list(
                 q=custom_url,
@@ -21,25 +22,21 @@ class YouTubeUserVideos(View):
                 maxResults=max_results
             ).execute()
 
-            ch = []
-            for item in resp['items']:
-                assert item['id']['kind'] == 'youtube#channel'
-                ch.append(item['id']['channelId'])
+            channel_ids = [item['id']['channelId'] for item in resp['items'] if item['id']['kind'] == 'youtube#channel']
 
-            if not len(ch):
+            if not channel_ids:
                 return None
 
             resp = self.youtube.channels().list(
-                id=','.join(ch),
+                id=','.join(channel_ids),
                 part='id,snippet',
                 fields='items(id,snippet(customUrl))',
-                maxResults=len(ch)
+                maxResults=len(channel_ids)
             ).execute()
 
             for item in resp['items']:
                 url = item['snippet'].get('customUrl')
-                if url is not None and url.lower() == custom_url.lower():
-                    assert item['id'] is not None
+                if url and url.lower() == custom_url.lower():
                     return item['id']
 
             return None
@@ -48,6 +45,7 @@ class YouTubeUserVideos(View):
             return JsonResponse({"error": f'An HTTP error {e.resp.status} occurred: {e.content}'}, status=400)
 
     def get(self, request, username):
+        """Handle GET requests to fetch YouTube videos for a given username."""
         try:
             channel_id = self.find_channel_by_custom_url(username)
             if channel_id is None:
@@ -55,14 +53,13 @@ class YouTubeUserVideos(View):
         except HttpError as e:
             return JsonResponse({"error": f'An HTTP error {e.resp.status} occurred: {e.content}'}, status=400)
 
-        request = self.youtube.search().list(
+        response = self.youtube.search().list(
             part='snippet',
             channelId=channel_id,
-            maxResults=25,  # fetches 25 videos, adjust this as needed
+            maxResults=25,  # fetches 25 videos
             type='video',  # to get only videos, not playlists or channels
             order='date'  # to get the most recent videos
-        )
-        response = request.execute()
+        ).execute()
 
         # Transform the data as needed
         processed_data = self.process_data(response['items'])
@@ -70,14 +67,12 @@ class YouTubeUserVideos(View):
         return JsonResponse(processed_data, safe=False)
 
     def process_data(self, data):
-        processed_data = []
-
-        for item in data:
-            video_data = {
+        """Process the raw data from YouTube API to a more convenient format."""
+        return [
+            {
                 'title': item['snippet']['title'],
                 'thumbnail': item['snippet']['thumbnails']['high']['url'],  # using the high resolution thumbnail
                 'link': f'https://www.youtube.com/watch?v={item["id"]["videoId"]}'
             }
-            processed_data.append(video_data)
-
-        return processed_data
+            for item in data
+        ]
